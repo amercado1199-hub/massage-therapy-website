ab499c77b5e1f6b21a105e90fe53f188148587a9
Alyssa Mercado <amercado1199@gmail.com>
2026-08-11 14:24:55 -0400

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,16 @@ function Home() {
         <div className="overlay"></div>
 
         <div className="hero-content minimalist-hero">
-          <div className="hero-logo large-logo">
+          <div className="hero-logo">
             <img
               src="/arce-logo.jpg"
               alt="A.R.C.E Massage Logo"
+              style={{ maxWidth: "400px", width: "100%", height: "auto" }}
             />
           </div>
 
           <div className="hero-buttons">
             <a href="/contact">
-            </a>
-
-            <a href="/services">
-              <button className="secondary-btn">
-                VIEW SERVICES
-              </button>
             </a>
           </div>
         </div>
